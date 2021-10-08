517fafb855c6913710839e5b70a9c60973b1cdaa
j3dec <jo.troadec@gmail.com>
2021-10-08 16:34:27 +0200

    Refactor global_amount

--- approche-objet/src/Operation/Operation.java
+++ approche-objet/src/Operation/Operation.java
@@ -6,6 +6,7 @@ public abstract class Operation {
 
     private String operation_date;
     private int op_amount;
+    private int global_amount = 0;
 
     public Operation(String operation_date, int op_amount) {
         this.operation_date = operation_date;
@@ -28,8 +29,16 @@ public abstract class Operation {
         this.op_amount = op_amount;
     }
 
+    public int getGlobal_amount() {
+        return global_amount;
+    }
+
+    public void setGlobal_amount(int global_amount) {
+        this.global_amount = global_amount;
+    }
+
     public String toString() {
-        return operation_date + " - " + op_amount;
+        return operation_date + " : " + op_amount;
     }
 
     public abstract String getType();
--- approche-objet/src/Operation/TestOperation.java
+++ approche-objet/src/Operation/TestOperation.java
@@ -9,24 +9,29 @@ public class TestOperation {
 
         Operation[] op_arr = new Operation[4];
 
-        op_arr[0] = new Credit("06/10/2021", 344);
-        op_arr[1] = new Debit("07/10/2021", 500);
-        op_arr[2] = new Credit("08/10/2021", 1839);
-        op_arr[3] = new Debit("08/10/2021", 3266);
+        op_arr[0] = new Credit("06/10/2021", 1865);
+        op_arr[1] = new Debit("07/10/2021", 2000);
+        op_arr[2] = new Credit("08/10/2021", 1200);
+        op_arr[3] = new Debit("08/10/2021", 234);
 
         int global_amount = 0;
         for (Operation operation : op_arr) {
             if (operation.getType() == "CREDIT") {
                 System.out.println(operation.getType());
                 global_amount += operation.getOp_amount();
+                operation.setGlobal_amount(global_amount);
+
             }
             if (operation.getType() == "DEBIT") {
                 System.out.println(operation.getType());
                 global_amount -= operation.getOp_amount();
+                operation.setGlobal_amount(global_amount);
+
             }
 
             System.out.println(operation);
             System.out.println("Montant Global : " + global_amount);
+            System.out.println("\n");
         }
     }
 }
